643bcc166ca9a3ca18303a96172c5741b85d5ce9
Rilwan <rilwannazar26@gmail.com>
2020-09-23 22:20:32 +0530

    updated files of robofriends

---
+++ src/robots.js
@@ -0,0 +1,80 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'Sivakumar',
+    username: 'Sivas',
+    email: 'sivakumarm@gmail.com'
+  },
+  {
+    id: 2,
+    name: 'Abinas',
+    username: 'Abi',
+    email: 'abinasn@gmail.com'
+  },
+  {
+    id: 3,
+    name: 'Parthiban',
+    username: 'Parthi',
+    email: 'parthibanm@gmail.com'
+  },
+  {
+    id: 4,
+    name: 'Abdul Rahman',
+    username: 'Abdul',
+    email: 'abdulaly@gmail.com'
+  },
+  {
+    id: 5,
+    name: 'Balachandar',
+    username: 'Saibala',
+    email: 'balachandarm@gmail.com'
+  },
+  {
+    id: 6,
+    name: 'Monisha',
+    username: 'Moni',
+    email: 'monisham@gmail.com'
+  },
+  {
+    id: 7,
+    name: 'Manoj',
+    username: 'Manoj',
+    email: 'manojd@gmail.com'
+  },
+  {
+    id: 8,
+    name: 'Jayasree',
+    username: 'Jayasree',
+    email: 'jayasreem@gmail.com'
+  },
+  {
+    id: 9,
+    name: 'Priyanka',
+    username: 'Priya',
+    email: 'priyankar@gmail.com'
+  },
+  {
+    id: 10,
+    name: 'Neethiyalagan',
+    username: 'Neethi',
+    email: 'neethib@gmail.com'
+  },
+  {
+    id: 11,
+    name: 'Madhubalan',
+    username: 'Madhu',
+    email: 'madhubalant@gmail.com'
+  },
+  {
+    id: 12,
+    name: 'Hariharan',
+    username: 'Hari',
+    email: 'haritony@gmail.com'
+  },
+  {
+    id: 13,
+    name: 'Rathesh',
+    username: 'Rathi',
+    email: 'ratheshr@gmail.com'
+  }
+];
